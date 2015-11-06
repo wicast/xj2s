b30def417eac8652ea45afecc9def5ace86f95a2
wicast <wicastchen@hotmail.com>
2015-11-07 02:00:26 +0800

    implemented a tire-tree.
    * basic insert operation is working.

---
+++ tireTree/tree.go
@@ -0,0 +1,59 @@
+package tireTree
+
+import (
+	"regexp"
+	"strings"
+)
+
+type TrieTreeNode struct {
+	NodeName PathName
+	Parent   *TrieTreeNode
+	Children map[PathName]*TrieTreeNode
+	Value    interface{}
+}
+
+type PathName struct {
+	Name   string
+	IsFlod bool
+}
+
+type TreePath []PathName
+
+func NewPaths(pathS string) ([]PathName, error) {
+	removeNum := regexp.MustCompile(`\[(\d+)\]`)
+	pathS = removeNum.ReplaceAllString(pathS, "[]")
+	splitedPath := strings.Split(pathS, ".")
+	var pN []PathName
+	for _, Node := range splitedPath {
+		IsFlod := false
+		if strings.Contains(Node, "[]") {
+			IsFlod = true
+		}
+		pN = append(pN, PathName{Name: Node, IsFlod: IsFlod})
+	}
+	return pN, nil
+}
+
+func (TT *TrieTreeNode) InsertNode(path TreePath, value interface{}) {
+	if Next, exist := TT.Children[path[0]]; !exist {
+		if len(path) == 1 {
+			ALeafNode := TT.insertSingleNode(path[0])
+			ALeafNode.Value = value
+		} else {
+			NewRouteNode := TT.insertSingleNode(path[0])
+			NewRouteNode.InsertNode(path[1:], value)
+		}
+	} else {
+		if len(path) != 1 {
+			Next.InsertNode(path[1:], value)
+		}
+	}
+
+}
+
+func (TT *TrieTreeNode) insertSingleNode(name PathName) *TrieTreeNode {
+	NewRouteNode := &TrieTreeNode{NodeName: name, Children: make(map[PathName]*TrieTreeNode)}
+	TT.Children[name] = NewRouteNode
+	NewRouteNode.Parent = TT
+	return NewRouteNode
+}
